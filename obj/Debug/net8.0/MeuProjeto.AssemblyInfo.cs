38985bba82c8548a215a74f06fcde380195eb5ff
vhrizzatti <huguinho.rizzatti@gmail.com>
2024-08-20 11:22:31 -0300

    gitignore

--- obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5015e318a2d13652650cd205fbd9049c8fec736")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

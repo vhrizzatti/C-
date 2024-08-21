534ef9a0250b7bbfe7ba5de424a0873b9fbe8709
vhrizzatti <huguinho.rizzatti@gmail.com>
2024-08-21 19:17:27 -0300

    Jogo Idade

--- obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc8459c37bc7f1e5d230ddb5b1e89654b3d809f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd97ad3c6d8b86c3b5688520698dbb6c67d67a3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
